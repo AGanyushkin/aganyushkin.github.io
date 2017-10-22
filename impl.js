ee90c6dc77a5aa9fd1e76fde3dd221ff535cc2e6
Andrey Ganyushkin <aganyushkin@hotmail.com>
2017-10-22 21:57:49 +0300

    new tests

--- impl.js
+++ impl.js
@@ -1,7 +1,7 @@
 
 function onload () {
     window.addEventListener("deviceorientation", handleOrientation, true);
-
+    window.addEventListener("devicemotion", handleMotion, true);    
 }
 
 function handleOrientation(event) {
@@ -10,10 +10,31 @@ function handleOrientation(event) {
     var beta     = event.beta;
     var gamma    = event.gamma;
 
-    document.getElementById('absolute').innerHTML = `${absolute}`;
-    document.getElementById('alpha').innerHTML = `${alpha}`;
-    document.getElementById('beta').innerHTML = `${beta}`;
-    document.getElementById('gamma').innerHTML = `${gamma}`;
+    document.getElementById('absolute').innerHTML = `absolute: ${absolute}`;
+    document.getElementById('alpha').innerHTML = `alpha: ${alpha}`;
+    document.getElementById('beta').innerHTML = `beta: ${beta}`;
+    document.getElementById('gamma').innerHTML = `gamma: ${gamma}`;
 
-    
+
+}
+
+function handleMotion(event) {
+    var acceleration = event.acceleration;
+    var accelerationIncludingGravity = event.accelerationIncludingGravity;
+    var rotationRate = event.rotationRate;
+    var interval = event.interval;
+
+    document.getElementById('ax').innerHTML = `ax: ${acceleration.x}`;
+    document.getElementById('ay').innerHTML = `ay: ${acceleration.y}`;
+    document.getElementById('az').innerHTML = `az: ${acceleration.z}`;
+
+    document.getElementById('axg').innerHTML = `ax+g: ${accelerationIncludingGravity.x}`;
+    document.getElementById('ayg').innerHTML = `ay+g: ${accelerationIncludingGravity.y}`;
+    document.getElementById('azg').innerHTML = `az+g: ${accelerationIncludingGravity.z}`;
+
+    document.getElementById('_alpha').innerHTML = `alpha: ${rotationRate.alpha}`;
+    document.getElementById('_beta').innerHTML = `beta: ${rotationRate.beta}`;
+    document.getElementById('_gamma').innerHTML = `gamma: ${rotationRate.gamma}`;
+
+    document.getElementById('interval').innerHTML = `interval: ${interval}`;
 }
